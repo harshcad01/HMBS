9542a75d591cb876bf26dab3870af26a13dd8670
harshcad01 <harshcad01@gmail.com>
2023-12-31 10:42:06 +0530

    js updated and forms updated

--- js/myscript.js
+++ js/myscript.js
@@ -1,59 +1,16 @@
-function add(){
-    var a=10;
-    var b=10;
-    if(a==b){
-        console.log("it is verified");
-    }else{
-        console.log("check the password");
-    }
-}
-function sub(){
-    let a=10;
-    let b= 30;
-    let c=a-b;
-    let d=a*b;
-    console.log("your a value is...." + a);   
-    console.log("your b value is...." + b);   
-    console.log("sutracted value is..."+ c);
-    console.log("multiplied value is..."+ d);
+function togglePassword() {
+    var passwordInput = document.getElementById("pass");
+    var confirmPasswordInput = document.getElementById("conpass");
+    var toggleCheckbox = document.getElementById("check");
 
-    if(c>a){
-        console.log("b is grater");
+    // Check if the checkbox is checked
+    if (toggleCheckbox.checked) {
+      // If checked, change the type attribute to "text" for both password fields
+      passwordInput.type = "text";
+      confirmPasswordInput.type = "text";
+    } else {
+      // If not checked, change it back to "password" for both password fields
+      passwordInput.type = "password";
+      confirmPasswordInput.type = "password";
     }
-    else{
-        console.log("a is grater");
-    }
-}
-
-function div(){
-    const a=10;   
-    const b=50;
-    const c=b/a;
-    console.log("your a value is...." + a);   
-    console.log("your b value is...." + b);   
-    console.log("divided value is..."+ c);
-}
-function all(){
-   let a=10;
-   var b=20;
-
-    console.log("added value...."+(a+b));
-    console.log(a-b);
-    console.log(a*b);
-    console.log(a/b);
-    console.log(a%b);
-    console.log(a**b);
-    console.log(b||a);
-    console.log(a&b);
-    console.log(a==b);
-    console.log(a===b);
-    console.log(a!==b);
-    console.log(a>b);
-    console.log(a<b);
-    console.log(a>=b);
-    console.log(a<=b);
-}
-add();
-sub();
-div();
-all();
+  }
