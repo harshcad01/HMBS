18f57ed6b533d342745857fe55ba013584096247
harshcad01 <harshcad01@gmail.com>
2024-01-03 14:40:42 +0530

    password is updated

--- js/myscript.js
+++ js/myscript.js
@@ -2,15 +2,23 @@ function togglePassword() {
     var passwordInput = document.getElementById("pass");
     var confirmPasswordInput = document.getElementById("conpass");
     var toggleCheckbox = document.getElementById("check");
+    var toggleCheckbox1 = document.getElementById("check1");
 
     // Check if the checkbox is checked
     if (toggleCheckbox.checked) {
       // If checked, change the type attribute to "text" for both password fields
       passwordInput.type = "text";
-      confirmPasswordInput.type = "text";
     } else {
       // If not checked, change it back to "password" for both password fields
       passwordInput.type = "password";
-      confirmPasswordInput.type = "password";
     }
+
+
+  if (toggleCheckbox1.checked) {
+    // If checked, change the type attribute to "text" for both password fields
+    confirmPasswordInput.type = "text";
+  } else {
+    // If not checked, change it back to "password" for both password fields
+    confirmPasswordInput.type = "password";
   }
+}
